fa8957baff77974e7080d8a49d521d8876b9cf8a
shaunmk <s.kandathil@ucl.ac.uk>
2019-04-26 15:06:05 +0100

    Improved test

--- test/testDMP.sh
+++ test/testDMP.sh
@@ -4,14 +4,28 @@
 
 ../run_DMP.sh -i 1guuA.fasta -a 1guuA.aln -m 1guuA.mtx
 
+L=$(tail -n 1 1guuA.fasta | wc -c)
+
 if [ $? == 0 ]; then
-    echo 'DMP test ran OK. DMP is ready to use.'
+    diff -q <(sort -rgk 5 1guuA.deepmetapsicov.con | head -n $L | cut -d ' ' -f 1-2) <(sort -rgk 5 example_con/1guuA.deepmetapsicov.con | head -n $L | cut -d ' ' -f 1-2)
+    ex=$?
+    if [ $ex == 0 ]; then
+	echo 'Top-L contacts match example output'
+    elif [ $ex == 1 ]; then
+	echo 'Mismatch in Top-L contact ranking; '
+    else
+	echo "Error in diff. Exit status was $ex . This shouldn't happen."
+    fi
+
+    echo 'DMP tests ran OK. DMP is ready to use.'
 else
     echo 'DMP test failed; please raise an issue on the GitHub page or email psipred@cs.ucl.ac.uk.'
+    exit 1
 fi
 
 # cleanup
-for ext in ss ss2 solv colstats pairstats ccmpred evfold psicov deepmetapsicov.21c deepmetapsicov.map deepmetapsicov.fix deepmetapsicov.con
-do
-    rm -f 1guuA.${ext}
-done
+
+# for ext in ss ss2 solv colstats pairstats ccmpred evfold psicov deepmetapsicov.21c deepmetapsicov.map deepmetapsicov.fix deepmetapsicov.con
+# do
+#     rm -f 1guuA.${ext}
+# done
